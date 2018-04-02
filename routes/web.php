a3229d06da8fa091a605400a9088cd9446eb9698
root <root@localhost.localdomain>
2018-04-02 14:17:41 +0900

    view and data binding

--- routes/web.php
+++ routes/web.php
@@ -23,28 +23,28 @@ Route::get('/', function () {
 	return '<h1>Hello Foo</h1>';
 });
 
-// 3-4
-Route::get('/{foo}', function ($foo) {
-	return $foo;
-});
+// // 3-4
+// Route::get('/{foo}', function ($foo) {
+// 	return $foo;
+// });
 
-// 3-5
-// /{foo}/profile 가능
-// /?foo=bar
-Route::get('/{foo?}', function ($foo = 'bar') {
-	return $foo;
-});
+// // 3-5
+// // /{foo}/profile 가능
+// // /?foo=bar
+// Route::get('/{foo?}', function ($foo = 'bar') {
+// 	return $foo;
+// });
 
-// 3-6
-Route::pattern('foo', '[0-9a-zA-Z]{3}');
-Route::get('/{foo?}', function ($foo = 'bar') {
-	return $foo;
-});
+// // 3-6
+// Route::pattern('foo', '[0-9a-zA-Z]{3}');
+// Route::get('/{foo?}', function ($foo = 'bar') {
+// 	return $foo;
+// });
 
-// 3-7
-Route::get('/{foo?}', function ($foo = 'bar') {
-	return $foo;
-})->where('foo', '[0-9a-zA-Z]{3}');
+// // 3-7
+// Route::get('/{foo?}', function ($foo = 'bar') {
+// 	return $foo;
+// })->where('foo', '[0-9a-zA-Z]{3}');
 
 // 3-8
 Route::get('/', [
@@ -57,3 +57,20 @@ Route::get('/home_22', function () {
 	return redirect(route('home_r'));
 });
 
+// 4-1
+Route::get('/', function () {
+	return view('errors.503');
+});
+
+// 4-2
+Route::get('/', function () {
+	return view('welcome')->with('name', 'Foo');
+});
+
+// 4-4
+Route::get('/', function () {
+	return view('welcome')->with([
+		'name' => 'Foo',
+		'greeting' => 'Hello ??',
+	]);
+});
