05a2a8260910c21104395702b46a5a333d777b47
root <root@localhost.localdomain>
2018-04-03 19:00:19 +0900

    입력갑 유효성 검사_수정

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -74,7 +74,7 @@ class ArticlesController extends Controller
         //     return back()->withErrors($validator)->withInput();
         // }
 
-        $this->validate($request, $rules, $messages);
+        // $this->validate($request, $rules, $messages);
 
         // $article = auth()->user()->articles()->create($request->all());
         $article = \App\User::find(1)->articles()->create($request->all());
@@ -82,6 +82,10 @@ class ArticlesController extends Controller
         if (! $article) {
             return back()->with('flash_message', '글이 저장되지 않았습니다.')->withInput();
         }
+
+        // var_dump('이벤트를 던집니다.');
+        // event('article.create', [$article]);
+        // var_dump('이벤트를 던졌습니다.');
 
         return redirect(route('articles.index'))->with('flash_message', '작성하신 글이 저장되었습니다.');
     }
